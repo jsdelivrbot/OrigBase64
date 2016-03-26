f099873503ccc3c8b8b3c5b228019bcd01b44bc2
Jorge Henriquez <george.enriquez52@gmail.com>
2016-03-26 14:39:26 -0700

    Add sixth,seventh,and eighth dight

--- script.js
+++ script.js
@@ -23,6 +23,10 @@ remainder262144b,
 remainder262144c,
 remainder262144d,
 remainder262144x,
+remainder262144y,
+remainder262144z,
+remainder262144z1,
+remainder262144z2,
 part4096a,
 part4096b,
 part4096c,
@@ -31,12 +35,19 @@ part262144b,
 part262144c,
 part262144d,
 part262144x,
+part262144y,
+part262144z,
+part262144z1,
+part262144z2,
 NewBase10,
 Base10A,
 Base10B,
 Base10C,
 Base10D,
 Base10X,
+Base10Y,
+Base10Z,
+Base10Z1,
 find10,
 find64,
 res10,
@@ -46,7 +57,7 @@ RandNum;
 
 // Generate Random Number between 0 and (64^4)-1
 function GenRand() {
-	amount = Math.pow(64,5)-1
+	amount = Math.pow(64,8)-1
 	RandNum = Math.floor(Math.random() * (amount - 0 + 1)) + 0;
 }
 
@@ -78,10 +89,36 @@ function IsMoreThan262144(number) {
 }
 
 function IsMoreThan16777216(number) {
-	if (number >= 16777216) {
+	if (number >= 16777216 && number < Math.pow(64,5)) {
 		return true;
 	}
 }
+
+function IsMoreThan64to5(number) {
+if (number >= Math.pow(64,5) && number < Math.pow(64,6)){
+		return true;
+	}
+}
+
+function IsMoreThan64to6(number) {
+	if (number >= Math.pow(64,6) && number < Math.pow(64,7)) {
+		return true;
+	}
+}
+
+function IsMoreThan64to7(number) {
+	if (number >= Math.pow(64,7) && number < Math.pow(64,8)){
+		return true;
+	}
+}
+
+function IsMoreThan64to8(number){
+	if (number >= Math.pow(64,8)) {
+		return true;
+	}
+}
+
+
 
 // Check if Random Number exists in existing array
 function CheckArray() {
@@ -99,7 +136,128 @@ function CheckArray() {
 
 // Convert to base 64 
 function GenBase64() {
-	if (IsMoreThan16777216(RandNum)) {
+	if (IsMoreThan64to8(RandNum)) {
+		remainder262144z2 = RandNum % Math.pow(64,8)
+		part262144z2 = Int[(RandNum - remainder262144z2)/Math.pow(64,8)];
+
+		remainder262144z1 = remainder262144z2 % Math.pow(64,7)
+		part262144z1 = Int[(remainder262144z2 - remainder262144z1)/Math.pow(64,7)];
+
+
+		remainder262144z = remainder262144z1 % Math.pow(64,6)
+		part262144z = Int[(remainder262144z1 - remainder262144z)/Math.pow(64,6)];
+
+
+		remainder262144y = remainder262144z % 1073741824
+		part262144y = Int[(remainder262144z - remainder262144y)/1073741824];
+
+		remainder262144x = remainder262144y % 16777216;
+		part262144x = Int[(remainder262144y - remainder262144x)/16777216];
+
+
+		remainder262144a = remainder262144x % 262144;
+		part262144a = Int[(remainder262144x - remainder262144a)/262144];
+
+		remainder262144b = remainder262144a % 4096;
+		part262144b = Int[(remainder262144a - remainder262144b)/4096];
+
+		remainder262144c = remainder262144b % 64;
+		part262144c = Int[(remainder262144b - remainder262144c)/64];
+
+		remainder262144d = remainder262144c % 1;
+		part262144d = Int[(remainder262144c - remainder262144d)/1];
+
+		NewNumber = part262144z+part262144y+part262144x+part262144a+part262144b+part262144c+part262144d;
+		push(RandNum,NewNumber);
+	}
+
+
+	else if (IsMoreThan64to7(RandNum)) {
+		remainder262144z1 = RandNum % Math.pow(64,7)
+		part262144z1 = Int[(RandNum - remainder262144z)/Math.pow(64,7)];
+
+
+		remainder262144z = remainder262144z1 % Math.pow(64,6)
+		part262144z = Int[(remainder262144z1 - remainder262144z)/Math.pow(64,6)];
+
+
+		remainder262144y = remainder262144z % 1073741824
+		part262144y = Int[(remainder262144z - remainder262144y)/1073741824];
+
+		remainder262144x = remainder262144y % 16777216;
+		part262144x = Int[(remainder262144y - remainder262144x)/16777216];
+
+
+		remainder262144a = remainder262144x % 262144;
+		part262144a = Int[(remainder262144x - remainder262144a)/262144];
+
+		remainder262144b = remainder262144a % 4096;
+		part262144b = Int[(remainder262144a - remainder262144b)/4096];
+
+		remainder262144c = remainder262144b % 64;
+		part262144c = Int[(remainder262144b - remainder262144c)/64];
+
+		remainder262144d = remainder262144c % 1;
+		part262144d = Int[(remainder262144c - remainder262144d)/1];
+
+		NewNumber = part262144z+part262144y+part262144x+part262144a+part262144b+part262144c+part262144d;
+		push(RandNum,NewNumber);
+	}
+	
+	 else if (IsMoreThan64to6(RandNum)) {
+		remainder262144z = RandNum % Math.pow(64,6)
+		part262144z = Int[(RandNum - remainder262144z)/Math.pow(64,6)];
+
+
+		remainder262144y = remainder262144z % 1073741824
+		part262144y = Int[(remainder262144z - remainder262144y)/1073741824];
+
+		remainder262144x = remainder262144y % 16777216;
+		part262144x = Int[(remainder262144y - remainder262144x)/16777216];
+
+
+		remainder262144a = remainder262144x % 262144;
+		part262144a = Int[(remainder262144x - remainder262144a)/262144];
+
+		remainder262144b = remainder262144a % 4096;
+		part262144b = Int[(remainder262144a - remainder262144b)/4096];
+
+		remainder262144c = remainder262144b % 64;
+		part262144c = Int[(remainder262144b - remainder262144c)/64];
+
+		remainder262144d = remainder262144c % 1;
+		part262144d = Int[(remainder262144c - remainder262144d)/1];
+
+		NewNumber = part262144z+part262144y+part262144x+part262144a+part262144b+part262144c+part262144d;
+		push(RandNum,NewNumber);
+	}
+
+
+	 else if (IsMoreThan64to5(RandNum)) {
+		remainder262144y = RandNum % 1073741824
+		part262144y = Int[(RandNum - remainder262144y)/1073741824];
+
+		remainder262144x = remainder262144y % 16777216;
+		part262144x = Int[(remainder262144y - remainder262144x)/16777216];
+
+
+		remainder262144a = remainder262144x % 262144;
+		part262144a = Int[(remainder262144x - remainder262144a)/262144];
+
+		remainder262144b = remainder262144a % 4096;
+		part262144b = Int[(remainder262144a - remainder262144b)/4096];
+
+		remainder262144c = remainder262144b % 64;
+		part262144c = Int[(remainder262144b - remainder262144c)/64];
+
+		remainder262144d = remainder262144c % 1;
+		part262144d = Int[(remainder262144c - remainder262144d)/1];
+
+		NewNumber = part262144y+part262144x+part262144a+part262144b+part262144c+part262144d;
+		push(RandNum,NewNumber);
+	}
+
+	else if (IsMoreThan16777216(RandNum)) {
 		remainder262144x = RandNum % 16777216;
 		part262144x = Int[(RandNum - remainder262144x)/16777216];
 
@@ -174,6 +332,39 @@ function GenBase10() {
 	array64 = numbers.split("");
 	DecimalPlaces = array64.length;
 	switch(DecimalPlaces){
+		case 8:
+			Base10Z1 = Int.indexOf(array64[0])*Math.pow(64, 7);
+			Base10Z = Int.indexOf(array64[1])*Math.pow(64, 6);
+			Base10Y = Int.indexOf(array64[2])*Math.pow(64, 5);
+			Base10X = Int.indexOf(array64[3])*Math.pow(64, 4);
+			Base10A = Int.indexOf(array64[4])*Math.pow(64, 3);
+			Base10B = Int.indexOf(array64[5])*Math.pow(64, 2);
+			Base10C = Int.indexOf(array64[6])*Math.pow(64, 1);
+			Base10D = Int.indexOf(array64[7])*Math.pow(64, 0);
+			NewBase10 = Base10Z1+Base10Z+Base10Y+Base10X+Base10A+Base10B+Base10C+Base10D;
+			push(NewBase10,numbers);
+			break;
+		case 7:
+			Base10Z = Int.indexOf(array64[0])*Math.pow(64, 6);
+			Base10Y = Int.indexOf(array64[1])*Math.pow(64, 5);
+			Base10X = Int.indexOf(array64[2])*Math.pow(64, 4);
+			Base10A = Int.indexOf(array64[3])*Math.pow(64, 3);
+			Base10B = Int.indexOf(array64[4])*Math.pow(64, 2);
+			Base10C = Int.indexOf(array64[5])*Math.pow(64, 1);
+			Base10D = Int.indexOf(array64[6])*Math.pow(64, 0);
+			NewBase10 = Base10Z+Base10Y+Base10X+Base10A+Base10B+Base10C+Base10D;
+			push(NewBase10,numbers);
+			break;
+		case 6:
+			Base10Y = Int.indexOf(array64[0])*Math.pow(64, 5);
+			Base10X = Int.indexOf(array64[1])*Math.pow(64, 4);
+			Base10A = Int.indexOf(array64[2])*Math.pow(64, 3);
+			Base10B = Int.indexOf(array64[3])*Math.pow(64, 2);
+			Base10C = Int.indexOf(array64[4])*Math.pow(64, 1);
+			Base10D = Int.indexOf(array64[5])*Math.pow(64, 0);
+			NewBase10 = Base10Y+Base10X+Base10A+Base10B+Base10C+Base10D;
+			push(NewBase10,numbers);
+			break;
 		case 5:
 			Base10X = Int.indexOf(array64[0])*Math.pow(64, 4);
 			Base10A = Int.indexOf(array64[1])*Math.pow(64, 3);
