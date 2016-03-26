2debbfe34d80299f926465361113863a9d4f0427
Jorge Henriquez <george.enriquez52@gmail.com>
2016-03-25 22:10:37 -0700

    Fix third digit

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function GenRand() {
 }
 
 function IsMoreThan64() {
-	  if (RandNum >= 64) {
+	  if (RandNum >= 64 && RandNum < 4096) {
     MoreThan64 = 1;
     }
 }
@@ -44,6 +44,7 @@ function IsMoreThan4096() {
 		MoreThan4096 = 1;
 	}
 }
+
 
 // push to array and display number
 function push() {
@@ -57,20 +58,8 @@ document.getElementById("new").innerHTML = "Base 64: "+ NewNumber;
 
 // If exists Generate other random number
 function GenBase64() {
-		
-		if (MoreThan4096 === 1) {
-			remainder4096a = RandNum % 4096;
-			part4096a = Int[((RandNum - remainder4096a)/4096)];
-			remainder4096b = remainder4096a % 64;
-			part4096b = Int[((remainder4096a - remainder4096b)/64)];
-			remainder4096c = remainder4096b % 1;
-			part4096c = Int[(remainder4096b - remainder4096c)/1];
-			NewNumber = part4096a+part4096b+part4096c;
-			push();
-		}
 
-
-		else if(MoreThan64 === 1) {
+		if(MoreThan64 === 1) {
 			remainder = RandNum % 64;
 			part1 = Int[(RandNum - remainder)/64];
 			part2 = Int[remainder]; 
@@ -84,6 +73,24 @@ else {
 	}
 
 }
+
+
+function GenBase64a() {
+	if (MoreThan4096 === 1) {
+			remainder4096a = RandNum % 4096;
+			part4096a = Int[(RandNum - remainder4096a)/4096];
+			remainder4096b = remainder4096a % 64;
+			part4096b = Int[(remainder4096a - remainder4096b)/64];
+			remainder4096c = remainder4096b % 1;
+			part4096c = Int[(remainder4096b - remainder4096c)/1];
+			NewNumber = part4096a+part4096b+part4096c;
+			push();
+		}
+}
+
+
+
+
 
 // Check if Random Number exists in existing array
 function CheckArray() {
@@ -102,8 +109,10 @@ function CheckArray() {
 function Gen() {
 	GenRand();
 	CheckArray();
+	IsMoreThan4096();
 	IsMoreThan64();
 	GenBase64();
+	GenBase64a();
 	document.getElementById("new1").innerHTML = "Base 10: "+ RandNum;
 	document.getElementById("array").innerHTML = Used_Int;
 	document.getElementById("array2").innerHTML = Used_Int64;
@@ -116,6 +125,7 @@ function GenFor() {
 	CheckArray();
 	IsMoreThan4096();
 	IsMoreThan64();
+	GenBase64a();
 	GenBase64();
 	document.getElementById("new1").innerHTML = "Base 10: "+ RandNum;
 	document.getElementById("array").innerHTML = Used_Int;
